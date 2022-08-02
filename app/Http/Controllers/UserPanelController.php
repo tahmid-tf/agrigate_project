2929874c650289eacefab89730483077f5c401ab
sumaiyaa-t <sumaiya@insurecow.com>
2022-08-03 02:40:32 +0600

    Modify Files

--- app/Http/Controllers/UserPanelController.php
+++ app/Http/Controllers/UserPanelController.php
@@ -6,15 +6,34 @@ use Illuminate\Http\Request;
 
 class UserPanelController extends Controller
 {
+    /*-------------------Home----------------------------*/
     public function index(){
         return view('user.homepage');
     }
+    public function Source(){
+        return view('user.source-from-us');
+    }
+    public function Work(){
+        return view('user.work-with-us');
+    }
+    /*--------------------------------------Home-----------*/
+
+    /*------------------- Solutions----------------------------*/
+
+    /* Solution for Farmers */
     public function solutions(){
         return view('user.solutions');
     }
+
+    /* Agrigate Center */
     public function AgrigateCenter(){
         return view('user.agrigate-center');
     }
+
+    /*------------------------------- Solutions-----------------*/
+
+    /*------------------- Financial Inclusion ----------------------------*/
+
     public function FinancialInclusion(){
         return view('user.financial-inclusion');
     }
@@ -27,15 +46,17 @@ class UserPanelController extends Controller
     public function ApplyForAgriInsurance(){
         return view('user.apply-for-agri-insurance');
     }
+
+    /*--------------------------------------- Financial Inclusion -------------*/
+
+    /*------------------- Team ----------------------------*/
+
     public function Team(){
         return view('user.team');
     }
-    public function Source(){
-        return view('user.source-from-us');
-    }
-    public function Work(){
-        return view('user.work-with-us');
-    }
+    /*---------------------------- Team ----------------------*/
+
+    /*------------------- Career ----------------------------*/
 
     public function Career(){
         return view('user.career');
@@ -43,4 +64,6 @@ class UserPanelController extends Controller
     public function ApplyNow(){
         return view('user.apply-now');
     }
+    /*------------------------------------------ Career ------*/
+
 }
